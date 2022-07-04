5f4b8d8f2978e240ede00f7ef92a71048ae92def
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-07-04 22:53:24 +0500

    ok

--- routes/components.js
+++ routes/components.js
@@ -371,103 +371,103 @@ router.get("/:id/get/comments", authorization, async (req, res) => {
 
 
 // //post comment  on product after order is delivered
-// router.post('commentsrating/:orderId', authorization, async (req, res) => {
-//   if (req.body.type === 'Component') {
-//     let product = await Component.findOne({ _id: req.params.id });
+router.post('/commentsrating/:orderId', authorization, async (req, res) => {
+  if (req.body.type === 'Component') {
+    let product = await Component.findOne({ _id: req.params.id });
 
-//     if (product) {
-//       const alreadyReviewed = product.comments.find(
-//         (r) => r.user.toString() === req.user._id.toString()
-//       );
+    if (product) {
+      const alreadyReviewed = product.comments.find(
+        (r) => r.user.toString() === req.user.name.toString()
+      );
 
-//       if (alreadyReviewed) {
-//         res.status(400).json('Product already reviewed');
-//       }
+      if (alreadyReviewed) {
+        res.status(400).json('Product already reviewed');
+      }
 
-//       let comment = new Comment({
-//         comment: req.body.comment,
-//         rating: req.body.rating,
-//         user: req.user.name,
-//         product: req.params.id,
-//       });
-//       await comment.save();
-//       product.comments.push(comment);
-//       product.numReviews = product.comments.length;
-//       product.rating =
-//         product.comments.reduce((acc, item) => item.rating + acc, 0) /
-//         product.comments.length;
-//       await product.save();
-//       console.log(comment);
-//       return res.json(comment);
-//     } else {
-//       res.status(404).json('Product not found');
-//     }
-//   } else if (req.body.type === 'Product') {
-//     {
-//       let product = await Product.findOne({ _id: req.params.id });
+      let comment = new Comment({
+        comment: req.body.comment,
+        rating: req.body.rating,
+        user: req.user.name,
+        product: req.params.id,
+      });
+      await comment.save();
+      product.comments.push(comment);
+      product.numReviews = product.comments.length;
+      product.rating =
+        product.comments.reduce((acc, item) => item.rating + acc, 0) /
+        product.comments.length;
+      await product.save();
+      console.log(comment);
+      return res.json(comment);
+    } else {
+      res.status(404).json('Product not found');
+    }
+  } else if (req.body.type === 'Product') {
+    {
+      let product = await Product.findOne({ _id: req.params.id });
 
-//       if (product) {
-//         const alreadyReviewed = product.comments.find(
-//           (r) => r.user.toString() === req.user._id.toString()
-//         );
+      if (product) {
+        const alreadyReviewed = product.comments.find(
+          (r) => r.user === req.user.name
+        );
 
-//         if (alreadyReviewed) {
-//           res.status(400).json('Product already reviewed');
-//         }
+        if (alreadyReviewed) {
+          res.status(400).json('Product already reviewed');
+        }
 
-//         let comment = new Comment({
-//           comment: req.body.comment,
-//           rating: req.body.rating,
-//           user: req.user.name,
-//           product: req.params.id,
-//         });
-//         await comment.save();
-//         product.comments.push(comment);
-//         product.numReviews = product.comments.length;
-//         product.rating =
-//           product.comments.reduce((acc, item) => item.rating + acc, 0) /
-//           product.comments.length;
-//         await product.save();
-//         console.log(comment);
-//         return res.json(comment);
-//       } else {
-//         res.status(404).json('Product not found');
-//       }
-//     }
-//   } else if (req.body.type === 'Custom') {
-//     {
-//       let product = await Product.findOne({ _id: req.params.id });
+        let comment = new Comment({
+          comment: req.body.comment,
+          rating: req.body.rating,
+          user: req.user.name,
+          product: req.params.id,
+        });
+        await comment.save();
+        product.comments.push(comment);
+        product.numReviews = product.comments.length;
+        product.rating =
+          product.comments.reduce((acc, item) => item.rating + acc, 0) /
+          product.comments.length;
+        await product.save();
+        console.log(comment);
+        return res.json(comment);
+      } else {
+        res.status(404).json('Product not found');
+      }
+    }
+  } else if (req.body.type === 'Custom') {
+    {
+      let product = await Product.findOne({ _id: req.params.id });
 
-//       if (product) {
-//         const alreadyReviewed = product.comments.find(
-//           (r) => r.user.toString() === req.user._id.toString()
-//         );
+      if (product) {
+        const alreadyReviewed = product.comments.find(
+          (r) => r.user === req.user.name
+        );
 
-//         if (alreadyReviewed) {
-//           res.status(400).json('Product already reviewed');
-//         }
+        if (alreadyReviewed) {
+          res.status(400).json('Product already reviewed');
+        }
 
-//         let comment = new Comment({
-//           comment: req.body.comment,
-//           rating: req.body.rating,
-//           user: req.user.name,
-//           product: req.params.id,
-//         });
-//         await comment.save();
-//         product.comments.push(comment);
-//         product.numReviews = product.comments.length;
-//         product.rating =
-//           product.comments.reduce((acc, item) => item.rating + acc, 0) /
-//           product.comments.length;
-//         await product.save();
-//         console.log(comment);
-//         return res.json(comment);
-//       } else {
-//         res.status(404).json('Product not found');
-//       }
-//     }
-//   }
-// });
+        let comment = new Comment({
+          comment: req.body.comment,
+          rating: req.body.rating,
+          user: req.user.name,
+          product: req.params.id,
+        });
+        await comment.save();
+        product.comments.push(comment);
+        product.numReviews = product.comments.length;
+        product.rating =
+          product.comments.reduce((acc, item) => item.rating + acc, 0) /
+          product.comments.length;
+        await product.save();
+        console.log(comment);
+        return res.json(comment);
+      } else {
+        res.status(404).json('Product not found');
+      }
+    }
+  }
+});
 
 
 
